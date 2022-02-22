87120a9764e46adae1a4e77fa903cb90cda030f4
a-tiny-developer <atinyflame@gmail.com>
2022-02-22 15:34:01 -0600

    animation Controller

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,14 +3,63 @@ import 'package:flash_chat_flutter/screens/screens.dart';
 import 'package:flash_chat_flutter/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   static const routeName = 'Welcome';
 
   @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    // animation = CurvedAnimation(
+    //   parent: controller,
+    //   curve: Curves.easeIn,
+    // );
+
+    animation = ColorTween(
+      begin: Colors.blueGrey,
+      end: Colors.white,
+    ).animate(controller);
+
+    controller.forward();
+
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: animation.value,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,7 +75,7 @@ class WelcomeScreen extends StatelessWidget {
                       image: Assets.images.logoPng,
                     ),
                   ),
-                  height: 60.0,
+                  height: 60,
                 ),
                 const Text(
                   'Flash Chat',
